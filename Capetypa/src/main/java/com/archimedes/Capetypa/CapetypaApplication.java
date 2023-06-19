94db0d67e1678cda47123c9e4ce9d413684e62ea
Archimedes Firmino <archimedessfirmino@gmail.com>
2023-06-19 20:48:02 -0300

    Base feita no Spring Initializr
    As dependências adicionadas foram o Spring Boot DevTools, Spring Web, H2 Database, Lombok e Spring Data JPA.

---
+++ Capetypa/src/main/java/com/archimedes/Capetypa/CapetypaApplication.java
@@ -0,0 +1,13 @@
+package com.archimedes.Capetypa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapetypaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapetypaApplication.class, args);
+	}
+
+}
---
+++ Capetypa/src/test/java/com/archimedes/Capetypa/CapetypaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archimedes.Capetypa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapetypaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
