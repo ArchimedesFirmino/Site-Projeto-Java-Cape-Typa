37b2d66c3535985556a490bba5f1b32e1f18bb7c
Archimedes Firmino <archimedessfirmino@gmail.com>
2023-07-10 19:09:53 -0300

    d

--- Capetypa/src/main/java/com/archimedes/capetypa/artigos/Artigo.java
+++ Capetypa/src/main/java/com/archimedes/capetypa/artigos/Artigo.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import jakarta.persistence.Table;
 
 @Entity
 public class Artigo {
